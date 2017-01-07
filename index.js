bcfc7acef365380fdeebeebe73594a92ca59ddb7
Pradeep Mishra <pradeep.mishra@raweng.com>
2017-01-07 13:28:32 +0530

    toCSV function updated
    toCSV function updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
 function toColumnArray(data, opts){
 
     opts = opts || { };
-    
+
     var delimiter   = (opts.delimiter || ',');
     var content     = data;
 
@@ -31,7 +31,7 @@ function toColumnArray(data, opts){
         if(item){
             item = item.split(delimiter);
             item.forEach(function(val, index){
-                hashData[headers[index]].push(trimQuote(val));
+                hashData[headers[index]].push(_trimQuote(val));
             });
         }
     });
@@ -56,7 +56,7 @@ function toObject(data, opts){
             item = item.split(delimiter);
             var hashItem = {};
             headers.forEach(function(headerItem, index){
-                hashItem[headerItem] = trimQuote(item[index]);
+                hashItem[headerItem] = _trimQuote(item[index]);
             });
             hashData.push(hashItem);
         }
@@ -84,7 +84,7 @@ function toSchemaObject(data, opts){
             item = item.split(delimiter);
             var schemaObject = {};
             item.forEach(function(val, index){
-                putDataInSchema(headers[index], val, schemaObject);
+                _putDataInSchema(headers[index], val, schemaObject);
             });
             hashData.push(schemaObject);
         }
@@ -92,7 +92,6 @@ function toSchemaObject(data, opts){
 
     return hashData;
 }
-
 
 function toArray(data, opts){
 
@@ -110,7 +109,7 @@ function toArray(data, opts){
     content.forEach(function(item){
         if(item){
             item = item.split(delimiter).map(function(cItem){
-                return trimQuote(cItem);
+                return _trimQuote(cItem);
             });
             arrayData.push(item);
         }
@@ -118,141 +117,7 @@ function toArray(data, opts){
     return arrayData;
 }
 
-function toCSV(data, opts){
-
-    opts                = opts || { };
-
-    opts.delimiter      = opts.delimiter || ',';
-
-    opts.wrap           = opts.wrap || '';
-
-    opts.arrayDenote    = opts.arrayDenote && String(opts.arrayDenote).trim() ? opts.arrayDenote : '[]';
-
-    opts.objectDenote   = opts.objectDenote && String(opts.objectDenote).trim() ? opts.objectDenote : '.';
-
-    opts.detailedOutput  = typeof(opts.detailedOutput) !== "boolean" ? true : opts.detailedOutput;
-
-    opts.headers  = String(opts.headers).toLowerCase();
-
-    if(!opts.headers.match(/none|full|relative|key/)){
-      opts.headers = 'full';
-    }else{
-      opts.headers = opts.headers.match(/none|full|relative|key/)[0];
-    }
-
-
-
-    var csvJSON         = { };
-    var csvData         = "";
-    var topLength       = 0;
-    var headers         = null;
-
-    if(opts.wrap === true){
-        opts.wrap = '"';
-    }
-
-    if(dataType(data) === "string"){
-        data = JSON.parse(data);
-    }
-
-
-    _toCSV(data, csvJSON, '', opts, true);
-
-    if(opts.wrap){
-        headers = Object.keys(csvJSON).map(function(i){
-            return opts.wrap + i + opts.wrap;
-        }).join(opts.delimiter) + '\n';
-    }else{
-        headers = Object.keys(csvJSON).join(opts.delimiter) + '\n';
-    }
-
-    csvData += opts.headers !== 'none' ? headers : '';
-
-    Object.keys(csvJSON).forEach(function(i){
-        if(Array.isArray(csvJSON[i]) && csvJSON[i].length > topLength){
-            topLength = csvJSON[i].length;
-        }
-    });
-
-    for(var i = 0; i < topLength; i++){
-        var thisLine = [ ];
-        Object.keys(csvJSON).forEach(function(j){
-            if(Array.isArray(csvJSON[j])){
-                if(opts.wrap){
-                    thisLine.push( opts.wrap + csvJSON[j][i] + opts.wrap);
-                }else{
-                    thisLine.push(csvJSON[j][i]);
-                }
-
-            }
-        });
-        csvData += thisLine.join(opts.delimiter) + '\n' ;
-    }
-
-    return csvData;
-
-}
-
-
-
-function putDataInSchema(header, item, schema){
-    var match = header.match(/\[*[\d]\]\.(\w+)|\.|\[\]|\[(.)\]|-|\+/ig);
-    var headerName, currentPoint;
-    if(match){
-        var testMatch = match[0];
-        if(match.indexOf('-') !== -1){
-            return true;
-        }else if(match.indexOf('.') !== -1){
-            var headParts = header.split('.');
-            currentPoint = headParts.shift();
-            schema[currentPoint] = schema[currentPoint] || {};
-            putDataInSchema(headParts.join('.'), item, schema[currentPoint]);
-        }else if(match.indexOf('[]') !== -1){
-            headerName = header.replace(/\[\]/ig,'');
-            if(!schema[headerName]){
-            schema[headerName] = [];
-            }
-            schema[headerName].push(item);
-        }else if(/\[*[\d]\]\.(\w+)/.test(testMatch)){
-            headerName = header.split('[').shift();
-            var index = parseInt(testMatch.match(/\[(.)\]/).pop(),10);
-            currentPoint = header.split('.').pop();
-            schema[headerName] = schema[headerName] || [];
-            schema[headerName][index] = schema[headerName][index] || {};
-            schema[headerName][index][currentPoint] = item;
-        }else if(/\[(.)\]/.test(testMatch)){
-            var delimiter = testMatch.match(/\[(.)\]/).pop();
-            headerName = header.replace(/\[(.)\]/ig,'');
-            schema[headerName] = convertArray(item, delimiter);
-        }else if(match.indexOf('+') !== -1){
-            headerName = header.replace(/\+/ig,"");
-            schema[headerName] = Number(item);
-        }
-    }else{
-        schema[header] = trimQuote(item);
-    }
-    return schema ;
-}
-
-
-
-
-function trimQuote(str){
-    return str.trim().replace(/^["|'](.*)["|']$/, '$1');
-}
-
-function convertArray(str, delimiter) {
-    var output = [];
-    var arr = str.split(delimiter);
-    arr.forEach(function(val) {
-        var trimmed = val.trim();
-        output.push(trimmed);
-    });
-    return output;
-}
-
-
-function dataType(arg) {
+function _dataType(arg) {
     if (arg === null) {
         return 'null';
     }
@@ -271,61 +136,209 @@ function dataType(arg) {
     return type;
 }
 
-function getKeyNameForObject(title, origin, key, opts){
-  if(opts.headers === 'key' || opts.headers === 'none'){
-    return key;
-  }else{
-    if(origin){
-      return (title ? title : '') + key;
+function toCSV(data, opts){
+
+    opts                = (opts || { });
+    opts.delimiter      = (opts.delimiter || ',');
+    opts.wrap           = (opts.wrap || '');
+    opts.arrayDenote    = (opts.arrayDenote && String(opts.arrayDenote).trim() ? opts.arrayDenote : '[]');
+    opts.objectDenote   = (opts.objectDenote && String(opts.objectDenote).trim() ? opts.objectDenote : '.');
+    opts.detailedOutput = (typeof(opts.detailedOutput) !== "boolean" ? true : opts.detailedOutput);
+    opts.headers        = String(opts.headers).toLowerCase();
+    var csvJSON         = { };
+    var csvData         = "";
+
+    if(!opts.headers.match(/none|full|relative|key/)){
+      opts.headers = 'full';
     }else{
-      return (title ? title + opts.objectDenote : '') + key;
+      opts.headers = opts.headers.match(/none|full|relative|key/)[0];
     }
-  }
-}
 
-function getKeyNameForArray(title, opts, contentIsObject){
-  if(contentIsObject && opts.headers === "relative"){
-     return "";
-  }
-  return title + opts.arrayDenote;
-}
+    if(opts.wrap === true){
+        opts.wrap = '"';
+    }
 
-function _toCSV(data, csv, title, opts, origin){
-    if(Array.isArray(data)){
-        data.some(function(i){
-            if(dataType(i) === 'string'){
-                _toCSV(
-                  data.join(';'),
-                  csv,
-                  getKeyNameForArray(title, opts, false),
-                  opts
-                );
-                return true;
+    if(typeof(data) === "string"){
+        data = JSON.parse(data);
+    }
+
+    _toCsv(data, csvJSON, "", 0, opts);
+
+    var headers = _getHeaders(opts.headers, csvJSON, opts);
+
+    if(headers){
+      if(opts.wrap){
+        headers = headers.map(function(item){
+          return opts.wrap + item + opts.wrap;
+        });
+      }
+      csvData = headers.join(opts.delimiter);
+    }
+
+    var bigArrayLen = _getBigArrayLength(csvJSON);
+    var keys        = Object.keys(csvJSON);
+    var row         = [ ];
+
+    var replaceNewLinePattern = /\n|\r/g;
+    if(!opts.wrap){
+        replaceNewLinePattern = new RegExp('\n|\r|' + opts.delimiter, 'g');
+    }
+
+    for(var i = 0; i < bigArrayLen; i++){
+        row = [ ];
+        for(var j = 0; j < keys.length; j++){
+            if(csvJSON[keys[j]][i]){
+                csvJSON[keys[j]][i] = csvJSON[keys[j]][i].replace(replaceNewLinePattern, '\t');
+                if(opts.wrap){
+                    csvJSON[keys[j]][i] = opts.wrap + csvJSON[keys[j]][i] + opts.wrap;
+                }
+                row[row.length] = csvJSON[keys[j]][i];
+            }else{
+                row[row.length] = "";
             }
-            return _toCSV(
-              i,
-              csv,
-              getKeyNameForArray(title, opts, true),
-              opts
-            );
-        });
-    }else if(data && dataType(data) === 'object'){
-        return Object.keys(data).forEach(function(key){
-            return _toCSV(
-              data[key],
-              csv,
-              getKeyNameForObject(title, origin, key, opts),
-              opts
-            );
-        });
-    }else{
-        if(typeof(data) === "object"){
-            data = String(data);
         }
-        if(csv[title]){
-            csv[title].push(data);
-        }else{
-            csv[title] = [ data ];
-        }
+      csvData += '\n' + row.join(opts.delimiter);
     }
+    return csvData;
+}
+
+
+function _toCsv(data, table, parent, row, opt){
+    if(_dataType(data) === 'undefined'){
+        return _putData('', table, parent, row, opt);
+    }else if(_dataType(data) === 'null'){
+        return _putData('null', table, parent, row, opt);
+    }else if(Array.isArray(data)){
+        return _arrayToCsv(data, table, parent, row, opt);
+    }else if(typeof(data) === "object"){
+        return _objectToCsv(data, table, parent, row, opt);
+    }else{
+        return _putData(String(data), table, parent, row, opt);
+    }
+}
+
+function _putData(data, table, parent, row, opt){
+  if(!table || !table[parent]){
+      table[parent] = [ ];
+  }
+  if(row < table[parent].length){
+    row = table[parent].length;
+  }
+  table[parent][row] = data;
+  return table;
+}
+
+function _arrayToCsv(data, table, parent, row, opt){
+    if(_doesNotContainsObjectAndArray(data)){
+      return _putData(data.join(';'), table, parent + opt.arrayDenote, row, opt);
+    }
+    data.forEach(function(item, index){
+        return _toCsv(item, table, parent + opt.arrayDenote, index, opt);
+    });
+}
+
+function _doesNotContainsObjectAndArray(array){
+  return array.every(function(item){
+        var datatype = _dataType(item);
+        if(!datatype.match(/array|object/)){
+          return true;
+        }
+        return false;
+  });
+}
+
+function _objectToCsv(data, table, parent, row, opt){
+  Object.keys(data).forEach(function(item){
+      return _toCsv(data[item], table, parent + opt.objectDenote + item, row, opt);
+  });
+}
+
+function _getHeaders(headerType, table, opt){
+  var keyMatchPattern       = /([\w\s_]+)$/;
+  var relativeMatchPattern  = /\[\]\.?([^\[\]]+)$/;
+  switch(headerType){
+    case "none":
+      return null;
+    case "full":
+      return Object.keys(table);
+    case "key":
+      return Object.keys(table).map(function(header){
+        var head = header.match(keyMatchPattern);
+        if(head && head.length === 2){
+          return head[1];
+        }
+        return header;
+      });
+    case "relative":
+      return Object.keys(table).map(function(header){
+        var head = header.match(relativeMatchPattern);
+        if(head && head.length === 2){
+          return head[1];
+        }
+        return header;
+      });
+  }
+}
+
+function _getBigArrayLength(table){
+  var len = 0;
+  Object.keys(table).forEach(function(item){
+      if(Array.isArray(table[item]) && item.length > len){
+        len = item.length;
+      }
+  });
+  return len;
+}
+
+function _putDataInSchema(header, item, schema){
+    var match = header.match(/\[*[\d]\]\.(\w+)|\.|\[\]|\[(.)\]|-|\+/ig);
+    var headerName, currentPoint;
+    if(match){
+        var testMatch = match[0];
+        if(match.indexOf('-') !== -1){
+            return true;
+        }else if(match.indexOf('.') !== -1){
+            var headParts = header.split('.');
+            currentPoint = headParts.shift();
+            schema[currentPoint] = schema[currentPoint] || {};
+            _putDataInSchema(headParts.join('.'), item, schema[currentPoint]);
+        }else if(match.indexOf('[]') !== -1){
+            headerName = header.replace(/\[\]/ig,'');
+            if(!schema[headerName]){
+            schema[headerName] = [];
+            }
+            schema[headerName].push(item);
+        }else if(/\[*[\d]\]\.(\w+)/.test(testMatch)){
+            headerName = header.split('[').shift();
+            var index = parseInt(testMatch.match(/\[(.)\]/).pop(),10);
+            currentPoint = header.split('.').pop();
+            schema[headerName] = schema[headerName] || [];
+            schema[headerName][index] = schema[headerName][index] || {};
+            schema[headerName][index][currentPoint] = item;
+        }else if(/\[(.)\]/.test(testMatch)){
+            var delimiter = testMatch.match(/\[(.)\]/).pop();
+            headerName = header.replace(/\[(.)\]/ig,'');
+            schema[headerName] = _convertArray(item, delimiter);
+        }else if(match.indexOf('+') !== -1){
+            headerName = header.replace(/\+/ig,"");
+            schema[headerName] = Number(item);
+        }
+    }else{
+        schema[header] = _trimQuote(item);
+    }
+    return schema ;
+}
+
+function _trimQuote(str){
+    return str.trim().replace(/^["|'](.*)["|']$/, '$1');
+}
+
+function _convertArray(str, delimiter) {
+    var output = [];
+    var arr = str.split(delimiter);
+    arr.forEach(function(val) {
+        var trimmed = val.trim();
+        output.push(trimmed);
+    });
+    return output;
 }
